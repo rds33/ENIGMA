5dc97e60465aa53f3801b49db9582e0c5d87faa7
rds33 <124891077+rds33@users.noreply.github.com>
2023-03-06 16:17:47 +0700

    Update challengerobby.php

--- challenge/challengerobby.php
+++ challenge/challengerobby.php
@@ -1,9 +1,44 @@
 <?php 
-    $names = array('Samuel','Stevano','Nanda','Febrianto', 'Andi', 'Jution');
+   $names = array('Samuel','Stevano','Nanda','Febrianto', 'Andi', 'Jution');
+    
 
-    sort($names, SORT_ASC);
+    // rsort($names,);
 
-    foreach($names as $key => $value){
-        echo $value . '<br>';
-    }
+    // foreach($names as $value){
+    // //     echo $value . '<br>';
+    // }
+    //$names=array(7,2,3,1,5,6);
+// Function bubble_sort($names){
+//         $n=count($names);
+//         for ($i = 0;$i<$n;$i++){ for ($j = $n-1;$j>$i;$j--){
+//                 if ($names[$j] < $names[$j-1]){
+//                     $dummy=$names[$j];
+//                     $names[$j]=$names[$j-1];
+//                     $names[$j-1]=$dummy;
+//                 }
+//             }
+//         }
+//         return $names;
+//     }
+//     (bubble_sort($names));
+
+//     foreach (bubble_sort($names) as $n) {
+//         echo $n . '<br>';
+//     }
+
+    //AKHIRNYA BISAAAAAAA!!!!!!!!
+
+    function insertion_sort($names){
+        $n=count($names);
+        for ($i = 1;$i<$n;$i++){ for ($k = $i; $k>0; $k--) {
+            if($names[$k]<$names[$k-1]){
+              $dummy=$names[$k];
+              $names[$k]=$names[$k-1];
+              $names[$k-1]=$dummy;
+            }
+          }
+        }
+        return $names;
+      }
+      print_r(insertion_sort($names));
 ?>
