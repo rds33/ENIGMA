d8a598c85606969c2482ae4389fec4a402338a19
rds33 <124891077+rds33@users.noreply.github.com>
2023-03-07 11:37:39 +0700

    Update challengerobby.php

--- challenge/challengerobby.php
+++ challenge/challengerobby.php
@@ -1,5 +1,4 @@
-<?php 
-   $names = array('Samuel','Stevano','Nanda','Febrianto', 'Andi', 'Jution');
+$names = array('Samuel','Stevano','Nanda','Febrianto', 'Andi', 'Jution');
     
 
     // rsort($names,);
@@ -8,6 +7,8 @@
     // //     echo $value . '<br>';
     // }
     //$names=array(7,2,3,1,5,6);
+
+    //ASCENDING
 // Function bubble_sort($names){
 //         $n=count($names);
 //         for ($i = 0;$i<$n;$i++){ for ($j = $n-1;$j>$i;$j--){
@@ -26,19 +27,38 @@
 //         echo $n . '<br>';
 //     }
 
-    //AKHIRNYA BISAAAAAAA!!!!!!!!
-
-    function insertion_sort($names){
-        $n=count($names);
-        for ($i = 1;$i<$n;$i++){ for ($k = $i; $k>0; $k--) {
-            if($names[$k]<$names[$k-1]){
-              $dummy=$names[$k];
-              $names[$k]=$names[$k-1];
-              $names[$k-1]=$dummy;
+// DESCENDING
+    Function bubble_sort($names){
+        $n=0;
+        for ($i = count($names);$i>$n;$i--){ for ($j = $n+1;$j<$i;$j++){
+                if ($names[$j] > $names[$j-1]){
+                    $dummy=$names[$j];
+                    $names[$j]=$names[$j-1];
+                    $names[$j-1]=$dummy;
+                }
             }
-          }
         }
         return $names;
-      }
-      print_r(insertion_sort($names));
+    }
+    (bubble_sort($names));
+
+    foreach (bubble_sort($names) as $n) {
+        echo $n . '<br>';
+    }
+    
+    //AKHIRNYA BISAAAAAAA!!!!!!!!
+
+    // function insertion_sort($names){
+    //     $n=count($names);
+    //     for ($i = $n;$i>$n;$i--){ for ($k = $i; $k<0; $k++) {
+    //         if($names[$k]<$names[$k+1]){
+    //           $dummy=$names[$k];
+    //           $names[$k]=$names[$k+1];
+    //           $names[$k-1]=$dummy;
+    //         }
+    //       }
+    //     }
+    //     return $names;
+    //   }
+    //   print_r(insertion_sort($names));
 ?>
